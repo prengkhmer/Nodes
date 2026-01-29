ca6326ccc5a899f5caf028e2a14def5520200850
Yin Khin <you@example.com>
2026-01-30 00:44:04 +0700

    ...

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,27 @@
 
+// const { Sequelize } = require("sequelize");
+
+// const sequelize = new Sequelize("ecommerce", "root", "", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   port: 3306,
+//   logging: false, 
+// });
+
+// module.exports = sequelize;
+
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("ecommerce", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-  logging: false, 
-});
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,
+  process.env.MYSQLUSER,
+  process.env.MYSQLPASSWORD,
+  {
+    host: process.env.MYSQLHOST,
+    port: Number(process.env.MYSQLPORT),
+    dialect: "mysql",
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
